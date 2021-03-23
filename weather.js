9864e13d70a497fbf455a84de85cea2a3a986f0f
Namukk <skadnr625@gmail.com>
2021-03-24 02:06:16 +0900

    weather

--- weather.js
+++ weather.js
@@ -1,9 +1,74 @@
+const weather = document.querySelector(".weatherContainer");
+
+const API_KEY = "4dbab2518d66804c19f6fa3b8b8e106e";
+const COORDS = "coords";
+
+const getWeather = async (lat, long) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((potato) => {
+      console.log(potato);
+    });
+};
+
+const saveCoords = (coordsObj) => {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+};
+
+const geoHandleSuccess = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+};
+
+const geoHandleError = () => {
+  console.log("Cannot get geoInfo");
+};
+
 const getGeo = () => {
-  navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  navigator.geolocation.getCurrentPosition(geoHandleSuccess, geoHandleError);
+};
+
+const functionWeather = () => {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    getGeo();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
 };
 
 function init() {
-  console.log(getGeo());
+  functionWeather();
 }
 
 init();
+
+// const COORDS = "coords";
+
+// const handleSuccess = (position) => {
+//   console.log(position);
+// };
+
+// const handleError = () => {
+//   console.log("Cannot get geo");
+// };
+
+// const askForCoords = () => {
+//   navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+// };
+
+// function init() {
+//   askForCoords();
+// }
+
+// init();
