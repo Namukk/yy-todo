1698eb272e1723c307550288f555dabd32aa15ed
Namukk <skadnr625@gmail.com>
2021-03-26 23:46:42 +0900

    delete todo

--- todo.js
+++ todo.js
@@ -2,52 +2,57 @@ const toDoForm = document.querySelector(".toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".toDoList");
 
-// const handleDelBtn = (event) => {
-//   if (event) {
-//     toDoList.removeChild(li);
-//   }
-// };
+const toDos = [];
 
-const paintToDo = (text) => {
+const toDoSave = () => {
+  localStorage.setItem("TODOS_LS", JSON.stringify(toDos));
+};
+
+const writeToDo = (text) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const delBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
   const handleDelBtn = (event) => {
     if (event) {
-      toDoList.removeChild(li);
+      localStorage.removeItem();
     }
   };
 
-  // delBtn.innerHTML = "❌";
   span.innerText = text;
   delBtn.innerHTML = "X";
   delBtn.addEventListener("click", handleDelBtn);
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  toDos.push(toDoObj);
+  toDoSave();
 };
-
-// function paintToDo(text) {
-//   const li = document.createElement("li");
-//   const delBtn = document.createElement("button");
-//   const span = document.createElement("span");
-
-//   delBtn.innerText = "X";
-
-//   span.innerText = text;
-//   li.appendChild(delBtn);
-//   li.appendChild(span);
-//   toDoList.appendChild(li);
-// }
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const currentValue = toDoInput.value;
-  paintToDo(currentValue);
+  writeToDo(currentValue);
   toDoInput.value = "";
 };
 
+const loadToDos = () => {
+  const loadedToDos = localStorage.getItem("TODOS_LS");
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach((a) => {
+      writeToDo(a.text);
+    });
+  }
+};
+
 function init() {
+  loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
