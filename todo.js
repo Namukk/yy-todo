656adb8bada2ab53115ceba34298c20388528d51
Namukk <skadnr625@gmail.com>
2021-03-27 23:43:55 +0900

    will do

--- todo.js
+++ todo.js
@@ -2,36 +2,86 @@ const toDoForm = document.querySelector(".toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".toDoList");
 
-const toDos = [];
+let toDos = [];
+let willDos = [];
 
 const toDoSave = () => {
   localStorage.setItem("TODOS_LS", JSON.stringify(toDos));
 };
 
+const willDoSave = () => {
+  localStorage.setItem("WILLDO_LS", JSON.stringify(willDos));
+};
+
+const handleDelBtn = (event) => {
+  const delLi = event.target.parentNode;
+  toDoList.removeChild(delLi);
+  const cleanToDos = toDos.filter((a) => {
+    return a.id !== parseInt(delLi.id);
+    //+delLi 해도 됨. ""delLi라고 하면 string화
+  });
+  toDos = cleanToDos;
+  toDoSave();
+};
+
+const handlePoBtn = (event) => {
+  const li = event.target.parentNode;
+  toDoList.removeChild(li);
+  const changeToDos = toDos.filter((a) => {
+    return a.id === parseInt(li.id);
+  });
+  willDos = willDos.concat(changeToDos);
+  willDoSave();
+};
+
 const writeToDo = (text) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const poBtn = document.createElement("button");
   const delBtn = document.createElement("button");
   const newId = toDos.length + 1;
   const toDoObj = {
     text: text,
     id: newId,
   };
-  const handleDelBtn = (event) => {
-    if (event) {
-      localStorage.removeItem();
-    }
-  };
 
   span.innerText = text;
   delBtn.innerHTML = "X";
+  poBtn.innerHTML = "↩️";
   delBtn.addEventListener("click", handleDelBtn);
+  poBtn.addEventListener("click", handlePoBtn);
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.appendChild(poBtn);
   li.id = newId;
   toDoList.appendChild(li);
   toDos.push(toDoObj);
   toDoSave();
+};
+
+const writeWillDo = (text) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const poBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+
+  span.innerText = text;
+  delBtn.innerHTML = "X";
+  poBtn.innerHTML = "↩️";
+  delBtn.addEventListener("click", handleDelBtn);
+  poBtn.addEventListener("click", handlePoBtn);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(poBtn);
+  li.id = newId;
+  willDoList.appendChild(li);
+  willDos.push(toDoObj);
+  willDoSave();
 };
 
 const handleSubmit = (event) => {
@@ -39,6 +89,16 @@ const handleSubmit = (event) => {
   const currentValue = toDoInput.value;
   writeToDo(currentValue);
   toDoInput.value = "";
+};
+
+const loadWillDos = () => {
+  const loadedWillDos = localStorage.getItem("WILLDOS_LS");
+  if (loadedWillDos !== null) {
+    const parsedWillDos = JSON.parse(loadedWillDos);
+    parsedWillDos.forEach((a) => {
+      writeWillDo(a.text);
+    });
+  }
 };
 
 const loadToDos = () => {
